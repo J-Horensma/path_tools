bcd33bd41e62e1c720c6f2b40413a25f0cb0e186
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-12 13:39:19 -0700

    Update path_tools.py

--- path_tools.py
+++ path_tools.py
@@ -1,50 +1,96 @@
-import os, sys, time, shutil
+import os, shutil, sys, platform, time
+from pathlib import Path
 
-#CONVERT BYTES, TO PROPER INCREMENTS, IN BINARY FORMAT
-def convert_bytes(BYTES):
-    if str(BYTES).strip() and isinstance(BYTES, int):
-        BINARY_INCREMENT = int(1024)
-        if BYTES < BINARY_INCREMENT:return f'{BYTES} b'
-        KILOBYTES = f'{round(BYTES/BINARY_INCREMENT, 2)}'
-        if BYTES >= BINARY_INCREMENT and BYTES < BINARY_INCREMENT ** 2:return f'{KILOBYTES} kb'
-        MEGABYTES = round(BYTES/(BINARY_INCREMENT ** 2), 2)
-        if BYTES >= (BINARY_INCREMENT ** 2) and BYTES < BINARY_INCREMENT ** 3:return f'{MEGABYTES} Mb'
-        GIGABYTES = round(BYTES/(BINARY_INCREMENT ** 3), 2)
-        if BYTES >= (BINARY_INCREMENT ** 3) and BYTES < BINARY_INCREMENT ** 4:return f'{GIGABYTES} Gb'
-        TERABYTES = round(BYTES/(BINARY_INCREMENT ** 4), 2)
-        return f'{TERABYTES} Tb'
-    elif not str(BYTES).strip():
-        raise EOFError('convert_bytes():\nAn empty variable, was supplied.')
-    elif not isinstance(BYTES, int):
-        raise TypeError('convert_bytes():\nA non-integer, was supplied.')
-    
-#LOOP THROUGH A SET OF KEYS AND REPLACE THEM,
-#WITH THEIR VALUES IN A STRING
-def replace_string_dictionary(STRING, DICTIONARY):
-    LAST_REPLACED_STRING = STRING
-    for OLD, NEW in DICTIONARY.items():
-        REPLACED_STRING = LAST_REPLACED_STRING.replace(OLD, NEW)
-        LAST_REPLACED_STRING = REPLACED_STRING
-    return REPLACED_STRING
-
-#FILTER QUOTES, FROM A PATH, STRIP ANY SPACES FROM IT, AND CHECK IF THE PATH EXISTS
-def filter(PATH):
-    FILTERED_PATH = os.path.normpath(replace_string_dictionary(PATH, {"'":'', '"':'', ' ':''}))                     
-    if FILTERED_PATH.strip('.') and os.path.exists(FILTERED_PATH):
-        return FILTERED_PATH
-    elif not str(PATH).strip():
-        raise EOFError('filter():\nAn empty variable, was supplied.')
+#RETURNS "True", IF THE SUPPLIED PATH,
+#HAS NO SPACES, OR THE OS-ACCEPTABLE QUOTING IS PRESENT,
+#OTHERWISE, "False"
+def is_safely_quoted(PATH):
+    STRIPPED = str(PATH).strip()
+    if STRIPPED and isinstance(STRIPPED, str):
+        OS_QUOTE = '"' if platform.system() == 'Windows' else "'"
+        PREPENDED_QUOTES = int(0)
+        for CHARACTER in STRIPPED:
+            if CHARACTER in ["'", '"']:
+                PREPENDED_QUOTES += 1
+            else:
+                break
+        if os.path.isabs(STRIPPED[1:-1]) and ' ' in STRIPPED and STRIPPED.startswith(OS_QUOTE) and STRIPPED.endswith(OS_QUOTE) and STRIPPED.count(OS_QUOTE) == int(2) and PREPENDED_QUOTES < int(2):
+            return True
+        elif ' ' not in STRIPPED and OS_QUOTE not in STRIPPED and PREPENDED_QUOTES == int(0):
+            return True
+        else:
+            return False
     else:
-        raise FileNotFoundError(f'filter():\nThe path: {FILTERED_PATH}, was not found.')
+        raise TypeError('is_safely_quoted():\nThe PATH variable, must be a string and cannot be empty')
+
+#FORMAT QUOTES IN THE SUPPLIED PATH,
+#TO MATCH THE OS FORMATTING, REMOVES PREPENDED QUOTES,
+#AS WELL AS THEIR MATCHING QUOTES, AT THE END OF THE PATH (IF EXISTENT),
+#REPLACES OS-APPROPRIATE QUOTES, WITH OS-APPROPRIATE QUOTES,
+#AND ADDS THE OS-APPROPRIATE QUOTES, IF THE PATH INCLUDES A SPACE
+def appropriate_quotes(PATH):
+    STRIPPED = str(PATH).strip()
+    if STRIPPED and isinstance(STRIPPED, str):
+        if is_safely_quoted(STRIPPED):
+            return STRIPPED
+        else:
+            NON_OS_QUOTE = "'" if platform.system() == 'Windows' else '"'
+            OS_QUOTE = '"' if platform.system() == 'Windows' else "'"
+            PREPENDED_QUOTES = int(0)
+            MATCHED_QUOTES = int(0)
+            for CHARACTER in STRIPPED:
+                if CHARACTER in ["'", '"']:
+                    PREPENDED_QUOTES += 1
+                    if CHARACTER == STRIPPED[-PREPENDED_QUOTES]:
+                        MATCHED_QUOTES += 1
+                else:
+                    break
+            STRIPPED = STRIPPED[PREPENDED_QUOTES:-MATCHED_QUOTES].replace(OS_QUOTE, NON_OS_QUOTE) if MATCHED_QUOTES > int(0) else STRIPPED[PREPENDED_QUOTES:].replace(OS_QUOTE, NON_OS_QUOTE)
+            APPROPRIATED = f'{OS_QUOTE}{STRIPPED}{OS_QUOTE}' if ' ' in STRIPPED else STRIPPED.replace(OS_QUOTE, NON_OS_QUOTE)
+            return APPROPRIATED
+    else:
+        raise TypeError('appropriate_quotes():\nThe PATH variable, must be a string and cannot be empty')
     
-#RECURSIVELY SCAN A FOLDER, FOR THE TOTAL FILES AND BYTES
+#UNQUOTE, THE SUPPLIED PATH
+def unquote_path(PATH):
+    STRIPPED = str(PATH).strip()
+    if STRIPPED and isinstance(STRIPPED, str):
+        OS_QUOTE = '"' if platform.system() == 'Windows' else "'"
+        if STRIPPED[0] == OS_QUOTE and STRIPPED[0] == STRIPPED[-1]:
+            return STRIPPED[1:-1]
+        else:
+            return STRIPPED
+    else:
+        raise TypeError('unquote_path():\nThe PATH variable, must be a string and cannot be empty')
+
+#UNQUOTE, STRIP, EXPAND VARIABLES, 
+#RESOLVE, AND REMOVE PREPENDED, PATHLIB PATH (IF ANY),
+#FROM THE SUPPLIED PATH
+def filter_path(PATH):
+    APPROPRIATELY_QUOTED = appropriate_quotes(PATH)
+    STRIPPED = unquote_path(APPROPRIATELY_QUOTED)
+    if STRIPPED and isinstance(STRIPPED, str):
+        PATHLIB_PATH = str(Path.cwd())
+        EXPANDED_VARIABLES = str(os.path.expandvars(STRIPPED))
+        RESOLVED = str(Path(EXPANDED_VARIABLES).resolve(strict=False))
+        STRIP_LENGTH = int(0) if int(len(PATHLIB_PATH)) == int(0) else int(len(PATHLIB_PATH)) + int(1)
+        if not os.path.isabs(EXPANDED_VARIABLES) and RESOLVED.lower().startswith(PATHLIB_PATH.lower()):
+            FILTERED = RESOLVED[STRIP_LENGTH:]
+        else:
+            FILTERED = RESOLVED
+        return FILTERED
+    else:
+        raise TypeError('filter_path():\nThe PATH variable, must be a string and cannot be empty')
+    
+#RECURSIVELY SCAN A FOLDER PATH, FOR THE TOTAL FILES AND BYTES
 def recursive_files_and_bytes_total(PATH):
-    if all([str(PATH).strip(), isinstance(PATH, str), os.path.isdir(PATH)]):
+    STRIPPED = filter_path(PATH)
+    if all([STRIPPED, isinstance(STRIPPED, str), os.path.exists(STRIPPED), os.path.isdir(STRIPPED)]):
         FILES_TOTAL = int(0)
         BYTES_TOTAL = int(0)
 
         #LOOP THROUGH FOLDERS, IN THE PATH, RECURSIVELY
-        for WALK_PATH, DIRECTORIES, FILES in os.walk(PATH):
+        for WALK_PATH, DIRECTORIES, FILES in os.walk(STRIPPED):
             
             #REMOVE HIDDEN AND LINK FILES, FROM FILES ARRAY
             FILES = [FILE for FILE in FILES if not FILE.startswith('.') and not os.path.islink(os.path.join(WALK_PATH, FILE))]
@@ -56,14 +102,36 @@ def recursive_files_and_bytes_total(PATH):
                 FILE_SIZE = int(os.path.getsize(SCAN_PATH))
                 BYTES_TOTAL += FILE_SIZE
         return FILES_TOTAL, BYTES_TOTAL
-    elif not str(PATH).strip():
-        raise EOFError('recursive_files_and_bytes_total():\nAn empty variable, was supplied.')
+    elif not STRIPPED or not isinstance(STRIPPED, str):
+        raise TypeError('recursive_files_and_bytes_total():\nThe PATH variable, must be a string and cannot be empty')
+    elif not os.path.exists(STRIPPED):
+        raise FileNotFoundError(f'recursive_files_and_bytes_total():\nThe path: {STRIPPED}\nwas not found')
     elif os.path.isfile(PATH):
-        raise NotADirectoryError('recursive_files_and_bytes_total():\nThe path, was not a folder.')
+        raise NotADirectoryError('recursive_files_and_bytes_total():\nThe PATH variable, must be a folder path')
+
+#CONVERT BYTES, TO PROPER INCREMENTS, IN BINARY FORMAT
+def convert_bytes(BYTES):
+    STRIPPED = str(BYTES).strip()
+    if STRIPPED and STRIPPED.isnumeric():
+        BYTES = int(STRIPPED)
+        BINARY_INCREMENT = int(1024)
+        if BYTES < BINARY_INCREMENT:return f'{BYTES} b'
+        KILOBYTES = f'{round(BYTES/BINARY_INCREMENT, 2)}'
+        if BYTES >= BINARY_INCREMENT and BYTES < BINARY_INCREMENT ** 2:return f'{KILOBYTES} kb'
+        MEGABYTES = round(BYTES/(BINARY_INCREMENT ** 2), 2)
+        if BYTES >= (BINARY_INCREMENT ** 2) and BYTES < BINARY_INCREMENT ** 3:return f'{MEGABYTES} Mb'
+        GIGABYTES = round(BYTES/(BINARY_INCREMENT ** 3), 2)
+        if BYTES >= (BINARY_INCREMENT ** 3) and BYTES < BINARY_INCREMENT ** 4:return f'{GIGABYTES} Gb'
+        TERABYTES = round(BYTES/(BINARY_INCREMENT ** 4), 2)
+        return f'{round(TERABYTES, 2)} Tb'
+    else:
+        raise TypeError('convert_bytes():\nThe BYTES variable, must be an integer and cannot be empty')
     
-#CONVERT SECONDS TO Y/M/W/D/H/M/S FORMAT
+#CONVERT SECONDS TO FULL TIME FORMAT
 def convert_seconds(SECONDS):
-    if all([str(SECONDS).strip(), isinstance(SECONDS, int)]):
+    STRIPPED = str(SECONDS).strip()
+    if STRIPPED and STRIPPED.isnumeric():
+        SECONDS = int(STRIPPED)
         YEARS = f'{SECONDS // int(31536000)}y:' if (SECONDS // int(31536000)) > int(0) else ''
         REMAINDER_SECONDS = SECONDS % int(31536000)
         MONTHS = f'{REMAINDER_SECONDS // int(2628000)}M:' if (REMAINDER_SECONDS // int(2628000)) > int(0) else ''
@@ -79,66 +147,78 @@ def convert_seconds(SECONDS):
         SECONDS = f'{REMAINDER_SECONDS % int(60)}s'
         CONVERTED_SECONDS = f'{YEARS}{MONTHS}{WEEKS}{DAYS}{HOURS}{MINUTES}{SECONDS}'
         return CONVERTED_SECONDS
-    elif not str(SECONDS).strip():
-        raise EOFError('convert_seconds():\nAn empty variable, was supplied.')
-    elif not isinstance(SECONDS, int):
-        raise TypeError('convert_seconds():\nA non-integer, was supplied.')
-
-#DISPLAY PROGRESS BAR, FOR THE "recursiveCopyWithProgress()" FUNCTION   
+    else:
+        raise TypeError('convert_seconds():\nThe SECONDS variable, must be an integer and cannot be empty')
+    
+#DISPLAY A PROGRESS BAR, FOR THE "recursive_copy_with_progress()" FUNCTION   
 def recursive_copy_progress_bar(COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS, LENGTH = int(30)):
-    if all([str(COPIED_BYTES).strip(), str(TOTAL_BYTES).strip(), str(ETA_SECONDS).strip(), isinstance(COPIED_BYTES, int), isinstance(TOTAL_BYTES, int), isinstance(ETA_SECONDS, int)]):
+    COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS = str(COPIED_BYTES).strip(), str(TOTAL_BYTES).strip(), str(ETA_SECONDS).strip()
+    if all([COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS, COPIED_BYTES.isnumeric(), TOTAL_BYTES.isnumeric(), ETA_SECONDS.isnumeric()]):
+        COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS = int(COPIED_BYTES), int(TOTAL_BYTES), int(ETA_SECONDS)
         PERCENT = (COPIED_BYTES / TOTAL_BYTES) * int(100)
         BARS_FILLED = (LENGTH * COPIED_BYTES) // TOTAL_BYTES
         PROGRESS_BAR = '=' * BARS_FILLED + '-' * (LENGTH - BARS_FILLED)
         ETA = convert_seconds(ETA_SECONDS)
         sys.stdout.write(f'\rProgress: [{PROGRESS_BAR}]({round(PERCENT)}%) ETA: {ETA.ljust(50)}')
         sys.stdout.flush()
-    elif not all([str(COPIED_BYTES).strip(), str(TOTAL_BYTES).strip(), str(ETA_SECONDS).strip()]):
-        raise EOFError('recursive_copy_progress_bar():\nOne or more empty variables, were supplied.')
-    elif not all([isinstance(COPIED_BYTES, int), isinstance(TOTAL_BYTES, int), isinstance(ETA_SECONDS, int)]):
-        raise TypeError('recursive_copy_progress_bar():\nOne or more non-integer variables, were supplied.')   
+    elif not all([COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS]):
+        raise EOFError('recursive_copy_progress_bar():\nOne or more empty variables, were supplied')
+    elif not all([COPIED_BYTES.isnumeric(), TOTAL_BYTES.isnumeric(), ETA_SECONDS.isnumeric()]):
+        raise TypeError('recursive_copy_progress_bar():\nOne or more non-integer variables, were supplied')   
     if PERCENT == int(100.0):
         print('\nFinished!')
 
-#RECURSIVELY COPY, A FOLDER PATH
+#RECURSIVELY COPY A FOLDER PATH, TO ANOTHER PATH,
+#WHILE SKIPPING NON-FILES, THAT CANNOT BE COPIED, LIKE SYM-LINKS
 def recursive_copy_with_progress(SOURCE_PATH, DESTINATION_PATH):
-    FILTERED_SOURCE_PATH = filter(SOURCE_PATH)
-    FILTERED_DESTINATION_PATH = os.path.join(filter(DESTINATION_PATH), os.path.basename(FILTERED_SOURCE_PATH))
-    TOTAL_FILES, TOTAL_BYTES = recursive_files_and_bytes_total(FILTERED_SOURCE_PATH)
-    COPIED_FILES = int(0)
-    COPIED_BYTES = int(0)
-    if os.path.isdir(FILTERED_SOURCE_PATH) and os.path.isdir(FILTERED_DESTINATION_PATH):
-        if FILTERED_SOURCE_PATH == FILTERED_DESTINATION_PATH or os.path.exists(FILTERED_DESTINATION_PATH):
-            i = int(0)
-            MAKE_DESTINATION_DIRECTORY = FILTERED_DESTINATION_PATH
-            while os.path.exists(MAKE_DESTINATION_DIRECTORY) == True:
-                i += int(1)
-                MAKE_DESTINATION_DIRECTORY = FILTERED_DESTINATION_PATH + ' ' + str(f'- Copy {i}').strip()
-            FILTERED_DESTINATION_PATH = MAKE_DESTINATION_DIRECTORY
-        os.makedirs(FILTERED_DESTINATION_PATH, exist_ok=True)
-        START_COPY = time.time()
-        for WALK_PATH, DIRECTORIES, FILES in os.walk(FILTERED_SOURCE_PATH):
-            RELATIVE_DIRECTORY = os.path.relpath(WALK_PATH, FILTERED_SOURCE_PATH)
-            SUB_DESTINATION_DIRECTORY = os.path.join(FILTERED_DESTINATION_PATH, RELATIVE_DIRECTORY)
-            os.makedirs(SUB_DESTINATION_DIRECTORY, exist_ok=True)
+    if str(SOURCE_PATH).strip() and str(DESTINATION_PATH).strip():
+        FILTERED_SOURCE_PATH = filter_path(SOURCE_PATH)
+        FILTERED_DESTINATION_PATH = filter_path(DESTINATION_PATH)
+        if os.path.exists(FILTERED_SOURCE_PATH) and os.path.exists(FILTERED_DESTINATION_PATH):
+            TOTAL_FILES, TOTAL_BYTES = recursive_files_and_bytes_total(FILTERED_SOURCE_PATH)
+            print(f'Copying a total of: {convert_bytes(TOTAL_BYTES)} and {TOTAL_FILES} files')
+            COPIED_FILES = int(0)
+            COPIED_BYTES = int(0)
+            if os.path.isdir(FILTERED_SOURCE_PATH) and os.path.isdir(FILTERED_DESTINATION_PATH):
+                FILTERED_DESTINATION_PATH = os.path.join(FILTERED_DESTINATION_PATH, os.path.basename(FILTERED_SOURCE_PATH))
+                if FILTERED_SOURCE_PATH == FILTERED_DESTINATION_PATH or os.path.exists(FILTERED_DESTINATION_PATH):
+                    i = int(0)
+                    MAKE_DESTINATION_DIRECTORY = FILTERED_DESTINATION_PATH
 
-            #REMOVE HIDDEN AND LINK FILES, FROM FILES ARRAY
-            FILES = [FILE_NAME for FILE_NAME in FILES if not FILE_NAME.startswith('.') and not os.path.islink(os.path.join(WALK_PATH, FILE_NAME))]
-            
-            for FILE_NAME in FILES:
-                SOURCE_FILE_PATH = os.path.join(WALK_PATH, FILE_NAME)
-                BYTES = int(os.path.getsize(SOURCE_FILE_PATH))
-                DESTINATION_FILE_PATH = os.path.join(SUB_DESTINATION_DIRECTORY, FILE_NAME)
-                shutil.copy2(SOURCE_FILE_PATH, DESTINATION_FILE_PATH)
+                    #CHECK IF THE FOLDER, ALREADY EXISTS, IN THE DESTINATION PATH
+                    #AND MAKE A NEW ONE, WITH A DIFFERENT NAME, IF SO
+                    while os.path.exists(MAKE_DESTINATION_DIRECTORY) == True:
+                        i += int(1)
+                        MAKE_DESTINATION_DIRECTORY = FILTERED_DESTINATION_PATH + ' ' + str(f'- Copy {i}').strip()
+                    FILTERED_DESTINATION_PATH = MAKE_DESTINATION_DIRECTORY
                 
-                COPY_COMPLETE = time.time()
-                ELAPSED_SECONDS = float(COPY_COMPLETE - START_COPY)
-                COPIED_FILES += int(1)
-                COPIED_BYTES += BYTES
-                BYTES_PER_SECOND = COPIED_BYTES / ELAPSED_SECONDS
-                ETA_SECONDS =  int(f'{(TOTAL_BYTES - COPIED_BYTES) / BYTES_PER_SECOND:.0f}')
-                
-                recursive_copy_progress_bar(COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS)
-        return
+                #MAKE THE FOLDER, IN THE DESTINATION PATH
+                os.makedirs(FILTERED_DESTINATION_PATH, exist_ok=True)
+                START_COPY = time.time()
+                for WALK_PATH, DIRECTORIES, FILES in os.walk(FILTERED_SOURCE_PATH):
+                    RELATIVE_DIRECTORY = os.path.relpath(WALK_PATH, FILTERED_SOURCE_PATH)
+                    SUB_DESTINATION_DIRECTORY = os.path.join(FILTERED_DESTINATION_PATH, RELATIVE_DIRECTORY)
+                    os.makedirs(SUB_DESTINATION_DIRECTORY, exist_ok=True)
+
+                    #REMOVE HIDDEN AND LINK FILES, FROM FILES ARRAY
+                    FILES = [FILE_NAME for FILE_NAME in FILES if not FILE_NAME.startswith('.') and not os.path.islink(os.path.join(WALK_PATH, FILE_NAME))]
+                    
+                    for FILE_NAME in FILES:
+                        SOURCE_FILE_PATH = os.path.join(WALK_PATH, FILE_NAME)
+                        BYTES = int(os.path.getsize(SOURCE_FILE_PATH))
+                        DESTINATION_FILE_PATH = os.path.join(SUB_DESTINATION_DIRECTORY, FILE_NAME)
+                        shutil.copy2(SOURCE_FILE_PATH, DESTINATION_FILE_PATH)
+                        COPY_COMPLETE = time.time()
+                        ELAPSED_SECONDS = float(COPY_COMPLETE - START_COPY)
+                        COPIED_FILES += int(1)
+                        COPIED_BYTES += BYTES
+                        BYTES_PER_SECOND = COPIED_BYTES / ELAPSED_SECONDS
+                        ETA_SECONDS =  int(f'{(TOTAL_BYTES - COPIED_BYTES) / BYTES_PER_SECOND:.0f}')
+                        recursive_copy_progress_bar(COPIED_BYTES, TOTAL_BYTES, ETA_SECONDS)
+                return
+            else:
+                raise NotADirectoryError('recursive_copy_with_progress():\nOne or more supplied paths, were not a folder')
+        else:
+            raise FileNotFoundError(f'recursive_copy_with_progress():\nOne or more supplied paths, were not found')
     else:
-        raise NotADirectoryError('recursive_copy_with_progress():\nOne or more paths, were not a folder.')
+        raise EOFError('recursive_copy_with_progress():\nOne or more empty variables, were supplied')
