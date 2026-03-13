a38f06819c38fbc74edd59ba3aa4340d0844f831
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-12 20:00:32 -0700

    Update recursive_copy_with_progress.py

--- Tests/recursive_copy_with_progress.py
+++ Tests/recursive_copy_with_progress.py
@@ -1,9 +1,6 @@
 from Library.path_tools import recursive_copy_with_progress
     
 try:
-    print('The "recursive_copy_with_progress()" function,')
-    print('recursively copies all files and folders, in the supplied source path,')
-    print('and displays a progress bar, with an ETA\n')
     SOURCE_PATH = input('Enter a source path: ')
     DESTINATION_PATH = input('Enter a destination path: ')
     recursive_copy_with_progress(SOURCE_PATH, DESTINATION_PATH)
