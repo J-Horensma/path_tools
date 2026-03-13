8099e4f9cf8928279a76be08e9ca37de0b42b7e8
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-13 02:56:06 -0700

    Update filter_path.py

--- Tests/filter_path.py
+++ Tests/filter_path.py
@@ -1,11 +1,10 @@
 from Library.path_tools import filter_path
 
 try:
-    print('The "filter_path()" function,')
-    print('filters and converts, the supplied path\n') 
     PATH = input('Enter a path: ')
     FILTERED = filter_path(PATH)
     print(f'Filtered path: {FILTERED}')
 except Exception as ERROR:
     print(ERROR)
 input('Press Enter: ')
+
