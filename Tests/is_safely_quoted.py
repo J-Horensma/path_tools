4d19f87dcd90b8966fc9a98535262ad59ae7f499
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-13 18:40:18 -0700

    Update is_safely_quoted.py

--- Tests/is_safely_quoted.py
+++ Tests/is_safely_quoted.py
@@ -1,9 +1,6 @@
 from Library.path_tools import is_safely_quoted
 
-try: 
-    print('The "is_safely_quoted()" function,')
-    print('returns True, if the supplied path,')
-    print('is correctly quoted (Or correctly not), otherwise False\n')
+try:
     PATH = input('Enter a path: ')
     print(f'Is safely quoted?: {is_safely_quoted(PATH)}')
 except Exception as ERROR:
