0d1ea3e9f301431b079925a8190496868ad34523
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-13 18:35:27 -0700

    Update convert_seconds.py

--- Tests/convert_seconds.py
+++ Tests/convert_seconds.py
@@ -1,8 +1,6 @@
 from Library.path_tools import convert_seconds
 
 try:
-    print('The "convert_seconds()" function,')
-    print('converts a number of seconds, to y:M:d:h:m:s format\n')
     SECONDS = input('Enter a number, of seconds: ')
     print(f'Converted: {convert_seconds(SECONDS)}')
 except Exception as ERROR:
