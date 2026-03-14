b94548d10bf5adbe3df3a7389cc2814ba1bdb88a
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-13 18:37:02 -0700

    Update appropriate_quotes.py

--- Tests/appropriate_quotes.py
+++ Tests/appropriate_quotes.py
@@ -1,14 +1,12 @@
 from Library.path_tools import is_safely_quoted, appropriate_quotes
 
-try: 
-    print('The "appropriate_quotes()" function,')
-    print('replaces and/or removes, incorrectly placed quotes, anywhere in the supplied path\n')
+try:
     PATH = input('Enter a path: ')
     if is_safely_quoted(PATH):
         print(f'Correct: {appropriate_quotes(PATH)}')
     else:
-        print(f'Incorrect: {PATH}')
-        print(f'Corrected: {appropriate_quotes(PATH)}')
+        print(f'Incorrectly quoted path: {PATH}')
+        print(f'Correctly quoted path: {appropriate_quotes(PATH)}')
 except Exception as ERROR:
     print(ERROR)
 input('Press Enter: ')
