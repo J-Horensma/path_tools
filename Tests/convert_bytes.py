426d29d1e5e2cfc77128fdc5ee54c51637ad4811
J-Horensma <62675940+J-Horensma@users.noreply.github.com>
2026-03-13 18:34:40 -0700

    Update convert_bytes.py

--- Tests/convert_bytes.py
+++ Tests/convert_bytes.py
@@ -1,8 +1,6 @@
 from Library.path_tools import convert_bytes
 
 try:
-    print('The "convert_bytes()" function,')
-    print('converts a number of bytes, to the correct format\n')
     BYTES = input('Enter a number, of bytes: ')
     print(f'Converted: {convert_bytes(BYTES)}')
 except Exception as ERROR:
